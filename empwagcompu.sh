29f1e265900b25f9bb574596b30c84cb31530f7d
unknown <narmada.vaddireddy97@gmail.com>
2023-02-03 21:47:15 +0530

    Daily Employee Wages

--- empwagcompu.sh
+++ empwagcompu.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+perHourSalary=20;
+workingHour=0;
+totalSalary=0;
 
-PRESENT=1;
-isPresent=$((RANDOM%2));
+for ((day=1; day<=20 ;day++))
+do
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
 
-if [ $isPresent -eq $PRESENT ]
-then
-	echo "Employee is Present";
-else
-	echo "Employee is Absent";
-fi
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+done
+
+echo "Employee has earned $totalSalary $ in a month";
